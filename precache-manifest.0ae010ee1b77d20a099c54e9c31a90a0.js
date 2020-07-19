76dd135453a7ceb8d8e08f4e5b2138c5d235107e
Jhonny Quispe <jhonny.quispejl@gmail.com>
2020-07-18 19:51:26 -0500

    Updates

---
+++ precache-manifest.0ae010ee1b77d20a099c54e9c31a90a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29bed7a7b420219afcffb89d20dc477a",
+    "url": "/react-firebase-appLink/index.html"
+  },
+  {
+    "revision": "5140533584a8145a3393",
+    "url": "/react-firebase-appLink/static/css/2.ea9217f3.chunk.css"
+  },
+  {
+    "revision": "cffdc2fd1977d1895e55",
+    "url": "/react-firebase-appLink/static/css/main.495324b1.chunk.css"
+  },
+  {
+    "revision": "5140533584a8145a3393",
+    "url": "/react-firebase-appLink/static/js/2.61079a17.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/react-firebase-appLink/static/js/2.61079a17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cffdc2fd1977d1895e55",
+    "url": "/react-firebase-appLink/static/js/main.98b38d38.chunk.js"
+  },
+  {
+    "revision": "15a35dc8c70fbfd18afa",
+    "url": "/react-firebase-appLink/static/js/runtime-main.52002330.js"
+  }
+]);
